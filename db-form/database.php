92f49edd53c2b56f732318ac457ffee854b27da3
godnames <niranjan.hospitalityminds@gmail.com>
2024-02-16 19:04:08 +0530

    update6

--- db-form/database.php
+++ db-form/database.php
@@ -1,3 +1,4 @@
+
 <?php
 $host        = "postgres://hospitality_minds_user:UgpLfXCRNSwC8UhuR5fA8GtEVUSeZkDV@dpg-cn6us0qcn0vc73dmov4g-a.singapore-postgres.render.com/hospitality_minds";
    $port        = "5432";
